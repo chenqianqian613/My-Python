a827b4641a8b06e56f17dc09a55dd31c75680dad
chenqianqian613 <61088405+chenqianqian613@users.noreply.github.com>
2020-03-20 11:03:13 +0800

    作业一

--- homework1/1.6.py
+++ homework1/1.6.py
@@ -4,11 +4,11 @@
 # @Author  : cqq
 #
 #前面2个元素为0，1，输出100以内的斐波那契数列；
-v1 = 0
-v2 = 1
-print(v1)
-print(v2)
+y1 = 0
+y2 = 1
+print(y1)
+print(y2)
 for i in range(1,100):
-    if i == v1 + v2:
+    if i == y1 + y2:
         print(i)
-        v1,v2 = v2,i
+        y1,y2 = y2,i
