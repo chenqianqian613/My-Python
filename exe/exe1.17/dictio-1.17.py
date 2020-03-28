fe6817b478092c4f66a12c19ff1dff66e0a3c6cd
chenqianqian613 <61088405+chenqianqian613@users.noreply.github.com>
2020-03-29 00:41:00 +0800

    Update dictio-1.17.py

--- exe/exe1.17/dictio-1.17.py
+++ exe/exe1.17/dictio-1.17.py
@@ -4,13 +4,17 @@
 # @Author  : cqq
 #
 #练习6：给定一个字典,保存了5个同学的学号,姓名,年龄;使用pickle模块将数据对象保存到文件中去;
+import os
 import pickle
-data1 = {'a': [1, 2.0, 3, 4+6j],
-         'b': ('string', u'Unicode string'),
-         'c': None}
-
-output = open(r'C:\Users\windows\Desktop\yyy\output', 'wb')
-
-# Pickle dictionary using protocol 0.
-pickle.dump(data1, output)
-output.close()
+data1 = [
+    {'num': '121', 'name': 'amy', 'age': 19},
+    {'num': '122', 'name': 'alice', 'age': 19},
+    {'num': '123', 'name': 'sam', 'age': 17},
+    {'num': '124', 'name': 'tom', 'age': 18},
+    {'num': '125', 'name': 'linda', 'age': 20}
+]
+with open('D:\My-Python\exe\exe1.17\pickle1', 'wb') as f:
+    pickle.dump(data1, f)
+with open('D:\My-Python\exe\exe1.17\pickle1', 'rb') as f:
+    ldata1 = pickle.load(f)
+    print(ldata1)
